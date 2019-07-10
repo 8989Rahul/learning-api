477ae7b939310bc9bee3666d2dc5134c6ec8b023
8989Rahul <daghalerahul0@gmail.com>
2019-07-10 07:40:17 +0530

    initializing leaning-api app

---
+++ src/components/ApiCall.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ApiCall extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Within the ApiCall</h1>
+      </div>
+    );
+  }
+}
