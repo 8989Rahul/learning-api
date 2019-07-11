af8f9436dffdbaceb92c4bdc0ea79e7c91f82c2e
8989Rahul <daghalerahul0@gmail.com>
2019-07-11 12:45:56 +0530

    import axios fetching reddit api display the title

--- src/components/ApiCall.jsx
+++ src/components/ApiCall.jsx
@@ -1,10 +1,42 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class ApiCall extends Component {
+  state = {
+    posts: [],
+    subr: "space"
+  };
+
+  componentWillMount() {
+    this.redditApi();
+  }
+
+  // https://www.reddit.com/r/space.json
+
+  redditApi = () => {
+    axios
+      .get(`https://www.reddit.com/r/${this.state.subr}.json`)
+      .then(result => {
+        console.log(result);
+
+        const posts = result.data.data.children.map(result => result.data);
+        console.log(posts);
+        console.log(posts.title);
+
+        this.setState({
+          posts: posts
+        });
+      });
+  };
   render() {
     return (
       <div>
-        <h1>Within the ApiCall</h1>
+        <h1>/r/{this.state.subr}</h1>
+        <ul className="list">
+          {this.state.posts.map(post => (
+            <li>{post.title}</li>
+          ))}
+        </ul>
       </div>
     );
   }
